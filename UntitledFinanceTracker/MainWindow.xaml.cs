6df97cae8eee8b926e337515743eb4e0ff4290e8
codybchaplin <chaplinbcody@gmail.com>
2022-01-26 17:35:02 -0500

    Added project files

--- UntitledFinanceTracker/MainWindow.xaml.cs
+++ UntitledFinanceTracker/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace UntitledFinanceTracker
 {
@@ -23,6 +12,71 @@ namespace UntitledFinanceTracker
         public MainWindow()
         {
             InitializeComponent();
+            lvMainMenu.SelectedIndex = 0;
+        }
+
+        /// <summary>
+        /// When a ListViewItem is selected, this event is triggered which opens the page corresponding to the selection.
+        /// </summary>
+        /// <param name="sender">Object that raised the event.</param>
+        /// <param name="e">Contains SelectionChanged event data.</param>
+        private void MainMenuSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            if (lvMainMenu != null)
+            {
+                if (ContentPanel != null)
+                    ContentPanel.Children.Clear();
+
+                // dashboard
+                if (lvMainMenu.SelectedItem.Equals(lviDashboard)) // if selected item is specified listViewItem
+                {
+                    Control ctrlDashboard = new Dashboard(); // insantiate control
+                    this.ContentPanel.Children.Add(ctrlDashboard); // add control to panel as child
+                }
+
+                // transaction view
+                if (lvMainMenu.SelectedItem.Equals(lviTransactionView))
+                {
+                    Control ctrlTransactionView = new TransactionView();
+                    this.ContentPanel.Children.Add(ctrlTransactionView);
+                }
+
+                // week view
+                if (lvMainMenu.SelectedItem.Equals(lviWeekView))
+                {
+                    Control ctrlWeekView = new WeekView();
+                    this.ContentPanel.Children.Add(ctrlWeekView);
+                }
+
+                // month view
+                if (lvMainMenu.SelectedItem.Equals(lviMonthView))
+                {
+                    Control ctrlMonthView = new MonthView();
+                    this.ContentPanel.Children.Add(ctrlMonthView);
+                }
+
+                // balance sheet
+                if (lvMainMenu.SelectedItem.Equals(lviBalanceSheet))
+                {
+                    Control ctrlBalanceSheet = new BalanceSheet();
+                    this.ContentPanel.Children.Add(ctrlBalanceSheet);
+                }
+
+                // statistics
+                if (lvMainMenu.SelectedItem.Equals(lviStats))
+                {
+                    Control ctrlStats = new Statistics();
+                    this.ContentPanel.Children.Add(ctrlStats);
+                }
+
+                // settings
+                if (lvMainMenu.SelectedItem.Equals(lviSettings))
+                {
+                    Control ctrlSettings = new Settings();
+                    this.ContentPanel.Children.Add(ctrlSettings);
+                }
+            }
         }
     }
 }
---
+++ UntitledFinanceTracker/Settings.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UntitledFinanceTracker
+{
+    /// <summary>
+    /// Interaction logic for Settings.xaml
+    /// </summary>
+    public partial class Settings : UserControl
+    {
+        public Settings()
+        {
+            InitializeComponent();
+            lvSettingsMenu.SelectedIndex = 0;
+        }
+
+        /// <summary>
+        /// When a ListViewItem is selected, this event is triggered which opens the page corresponding to the selection.
+        /// </summary>
+        /// <param name="sender">Object that raised the event.</param>
+        /// <param name="e">Contains SelectionChanged event data.</param>
+        private void SettingsMenuSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lvSettingsMenu != null)
+            {
+                if (SettingsPanel != null)
+                    SettingsPanel.Children.Clear();
+
+                // general
+                if (lvSettingsMenu.SelectedItem.Equals(lviGeneral)) // if selected item is specified listViewItem
+                {
+                    Control ctrlSettingsGeneral = new SettingsGeneral(); // insantiate control
+                    SettingsPanel.Children.Add(ctrlSettingsGeneral); // add control to panel as child
+                }
+
+                // account types
+                if (lvSettingsMenu.SelectedItem.Equals(lviAccountTypes))
+                {
+                    Control ctrlSettingsAccountTypes = new SettingsAccountTypes();
+                    SettingsPanel.Children.Add(ctrlSettingsAccountTypes);
+                }
+
+                // accounts
+                if (lvSettingsMenu.SelectedItem.Equals(lviAccounts))
+                {
+                    Control ctrlSettingsAccounts = new SettingsAccounts();
+                    this.SettingsPanel.Children.Add(ctrlSettingsAccounts);
+                }
+
+                // categories
+                if (lvSettingsMenu.SelectedItem.Equals(lviCategories))
+                {
+                    Control ctrlSettingsCategories = new SettingsCategories();
+                    this.SettingsPanel.Children.Add(ctrlSettingsCategories);
+                }
+            }
+        }
+    }
+}

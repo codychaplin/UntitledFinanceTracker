1cbbc00bd49487c8fd34d001af29e24479dfb5b6
codybchaplin <chaplinbcody@gmail.com>
2022-05-16 14:36:32 -0400

    Added income/expense column chart

--- UntitledFinanceTracker/Views/Dashboard.xaml.cs
+++ UntitledFinanceTracker/Views/Dashboard.xaml.cs
@@ -8,6 +8,8 @@ using LiveCharts.Helpers;
 using LiveCharts.Geared;
 using System.Windows;
 using System.Windows.Media;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace UntitledFinanceTracker.Views
 {
@@ -16,7 +18,6 @@ namespace UntitledFinanceTracker.Views
     /// </summary>
     public partial class Dashboard : UserControl
     {
-        public string[] Labels { get; set; }
         DateTime fromDate { get; set; }
         DateTime ToDate { get; set; }
 
@@ -43,17 +44,33 @@ namespace UntitledFinanceTracker.Views
         }
 
         /// <summary>
-        /// Updates running balance chart.
+        /// Updates all charts.
         /// </summary>
-        void UpdateRunningBalanceChart()
+        void UpdateCharts()
         {
-            // cache start and end dates, get list of properties from Transactions
+            // cache dates
             fromDate = dtFrom.SelectedDate.Value;
             ToDate = dtTo.SelectedDate.Value;
-            var results = Data.Transactions.Where(t => t.Date >= fromDate && t.Date <= ToDate)
+
+            // get list of Transactions within date, excluding transfers, and ordered by Order
+            var query = Data.Transactions.Where(t => t.Date >= fromDate && t.Date <= ToDate)
                                            .Where(t => t.CategoryID != Data.TRANSFER_ID)
-                                           .OrderBy(t => t.Order)
-                                           .Select(t => new { Date = t.Date, Amount = t.Amount, Balance = t.Balance }).ToList();
+                                           .OrderBy(t => t.Order);
+
+            // update charts
+            UpdateRunningBalanceChart(query);
+            UpdateExpensesChart(query);
+            UpdateIncomeExpensesChart(query);
+        }
+
+        /// <summary>
+        /// Updates running balance chart.
+        /// </summary>
+        /// <param name="query">Filtered list of Transactions.</param>
+        void UpdateRunningBalanceChart(IOrderedEnumerable<Transaction> query)
+        {
+            // get list of dates and balances from Transactions
+            var results = query.Select(t => new { Date = t.Date, Balance = t.Balance }).ToList();
 
             // split results
             string[] dates = results.Select(r => r.Date.ToString("yy-MM-dd")).ToArray();
@@ -71,38 +88,30 @@ namespace UntitledFinanceTracker.Views
                     LineSmoothness = 0.5
                 }
             };
+
             chrtBalance.Series = seriesCollection;
 
             // formats Y axis values as currency
             Func<double, string> YFormatter = value => value.ToString("C0");
             chrtBalanceAxisY.LabelFormatter = YFormatter;
 
-            // sets X axis values to dates
-            Labels = dates;
-            chrtBalanceAxisX.Labels = Labels;
+            chrtBalanceAxisX.Labels = dates; // sets X axis values to dates
         }
 
         /// <summary>
         /// Updates expenses chart.
         /// </summary>
-        void UpdateExpensesChart()
+        /// <param name="query">Filtered list of Transactions.</param>
+        void UpdateExpensesChart(IOrderedEnumerable<Transaction> query)
         {
-            // cache start and end dates
-            fromDate = dtFrom.SelectedDate.Value;
-            ToDate = dtTo.SelectedDate.Value;
-            
             // get transactions between dates, group by category, return objects containing category name and summed amount
-            var query = Data.Transactions.Where(t => t.Date >= fromDate && t.Date <= ToDate)
-                                         .Where(t => t.CategoryID > Data.INCOME_ID)
-                                         .GroupBy(t => t.CategoryName)
-                                         .Select(t => new
-                                         {
-                                             Category = t.First().CategoryName,
-                                             Amount = t.Sum(a => a.Amount)
-                                         });
+            var results = query.Where(t => t.CategoryID > Data.INCOME_ID)
+                             .GroupBy(t => t.CategoryName)
+                             .Select(t => new { Category = t.First().CategoryName,
+                                                Amount = t.Sum(a => a.Amount) });
             
             SeriesCollection seriesCollection = new();
-            foreach (var item in query)
+            foreach (var item in results)
             {
                 seriesCollection.Add(
                     new PieSeries
@@ -115,10 +124,59 @@ namespace UntitledFinanceTracker.Views
                     });
             };
 
-            // sets balances as chart values in series
             chrtExpenses.Series = seriesCollection;
 
+            // on hover, only show details for selection
             (chrtExpenses.DataTooltip as DefaultTooltip).SelectionMode = TooltipSelectionMode.OnlySender;
+        }
+
+        /// <summary>
+        /// Updates Incomes/Expenses chart.
+        /// </summary>
+        /// <param name="query">Filtered list of Transactions.</param>
+        void UpdateIncomeExpensesChart(IOrderedEnumerable<Transaction> query)
+        {
+            // split query into incomes/expenses grouped by month
+            var incomes = query.Where(t => t.CategoryID == Data.INCOME_ID)
+                               .GroupBy(t => t.Date.Month)
+                               .Select(t => t.Sum(t => t.Amount)).AsChartValues();
+            // select month as well for axis label
+            var expenses = query.Where(t => t.CategoryID > Data.INCOME_ID)
+                                .GroupBy(t => t.Date.Month)
+                                .Select(t => new { Month = t.First().Date, Amount = t.Sum(t => t.Amount) });
+
+            // flip expense sums to positive unless already positive, in that case make them negative
+            var expenseBalances = expenses.Select(i => (i.Amount <= 0) ? Math.Abs(i.Amount) : i.Amount * -1).AsChartValues();
+            var dates = expenses.Select(i => i.Month.ToString("MMM yy")).ToList();
+
+            var converter = new BrushConverter();
+
+            SeriesCollection seriesCollection = new()
+            {
+                new ColumnSeries // income series
+                {
+                    Title = "Income",
+                    Values = incomes,
+                    Fill = (Brush)converter.ConvertFromString("#54de42")
+                },
+                new ColumnSeries // expense series
+                {
+                    Title = "Expense",
+                    Values = expenseBalances,
+                    Fill = Brushes.Red
+                }
+            };
+
+            chrtIncExp.Series = seriesCollection;
+
+            // formats Y axis values as currency
+            Func<double, string> YFormatter = value => value.ToString("C0");
+            chrtIncExpAxisY.LabelFormatter = YFormatter;
+
+            chrtIncExpAxisX.Labels = dates; // sets X axis values to dates
+
+            // on hover, only show details for selection
+            (chrtIncExp.DataTooltip as DefaultTooltip).SelectionMode = TooltipSelectionMode.OnlySender;
         }
 
         private void dtFrom_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
@@ -131,8 +189,8 @@ namespace UntitledFinanceTracker.Views
                 if (dtFrom.SelectedDate >= dtTo.SelectedDate)
                     throw new Exception("Error: \"From\" date must be before \"To\" date.");
 
-                UpdateRunningBalanceChart();
-                UpdateExpensesChart();
+                UpdateCharts();
+                
             }
             catch (Exception ex)
             {
@@ -153,8 +211,7 @@ namespace UntitledFinanceTracker.Views
                 if (dtFrom.SelectedDate >= dtTo.SelectedDate)
                     throw new Exception("Error: \"From\" date must be before \"To\" date.");
 
-                UpdateRunningBalanceChart();
-                UpdateExpensesChart();
+                UpdateCharts();
             }
             catch (Exception ex)
             {
